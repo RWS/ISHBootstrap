d23df5304b0ea95889cdf97b4208ea5387faa465
Alex Sarafian <dev1978@outlook.com>
2017-05-10 14:42:11 +0200

    GH-53 : Fixed issues

--- Source/Docker/ISH.Cmd.ps1
+++ Source/Docker/ISH.Cmd.ps1
@@ -1,30 +1,30 @@
 param(
-    [Parameter(Mandatory=$true,ParameterSet="External Database")]
-    [Parameter(Mandatory=$true,ParameterSet="Internal Database")]
+    [Parameter(Mandatory=$true,ParameterSetName="External Database")]
+    [Parameter(Mandatory=$true,ParameterSetName="Internal Database")]
     [string]$OsUserName,
-    [Parameter(Mandatory=$true,ParameterSet="External Database")]
-    [Parameter(Mandatory=$true,ParameterSet="Internal Database")]
+    [Parameter(Mandatory=$true,ParameterSetName="External Database")]
+    [Parameter(Mandatory=$true,ParameterSetName="Internal Database")]
     [string]$OsUserPassword,
-    [Parameter(Mandatory=$true,ParameterSet="External Database")]
-    [Parameter(Mandatory=$true,ParameterSet="Internal Database")]
+    [Parameter(Mandatory=$true,ParameterSetName="External Database")]
+    [Parameter(Mandatory=$true,ParameterSetName="Internal Database")]
     [string]$PFXCertificatePath,
-    [Parameter(Mandatory=$true,ParameterSet="External Database")]
-    [Parameter(Mandatory=$true,ParameterSet="Internal Database")]
-    [securestring]$PFXCertificatePassword,
-    [Parameter(Mandatory=$false,ParameterSet="External Database")]
-    [Parameter(Mandatory=$false,ParameterSet="Internal Database")]
+    [Parameter(Mandatory=$true,ParameterSetName="External Database")]
+    [Parameter(Mandatory=$true,ParameterSetName="Internal Database")]
+    [string]$PFXCertificatePassword,
+    [Parameter(Mandatory=$false,ParameterSetName="External Database")]
+    [Parameter(Mandatory=$false,ParameterSetName="Internal Database")]
     [string]$HostName=$null,
-    [Parameter(Mandatory=$true,ParameterSet="External Database")]
+    [Parameter(Mandatory=$true,ParameterSetName="External Database")]
     [string]$ConnectionString,
-    [Parameter(Mandatory=$true,ParameterSet="External Database")]
+    [Parameter(Mandatory=$true,ParameterSetName="External Database")]
     [ValidateSet("sqlserver2014","oracle")]
     [string]$DBType,
-    [Parameter(Mandatory=$false,ParameterSet="Internal Database")]
+    [Parameter(Mandatory=$false,ParameterSetName="Internal Database")]
     [string]$sa_password,
-    [Parameter(Mandatory=$false,ParameterSet="Internal Database")]
+    [Parameter(Mandatory=$false,ParameterSetName="Internal Database")]
     [string]$ACCEPT_EULA,
-    [Parameter(Mandatory=$false,ParameterSet="External Database")]
-    [Parameter(Mandatory=$false,ParameterSet="Internal Database")]
+    [Parameter(Mandatory=$false,ParameterSetName="External Database")]
+    [Parameter(Mandatory=$false,ParameterSetName="Internal Database")]
     [switch]$Loop=$false
 )
 
@@ -35,6 +35,7 @@ if ($PSBoundParameters['Debug']) {
 $buildersPath=Join-Path $PSScriptRoot "..\Builders"
 
 $osUserCredentials=New-Object System.Management.Automation.PSCredential($OsUserName, (ConvertTo-SecureString -String $OsUserPassword -AsPlainText -Force))
+$osUserCredentials=Get-ISHNormalizedCredential -Credentials $osUserCredentials
 $pfxCertificateSecurePassword=ConvertTo-SecureString -String $PFXCertificatePassword -AsPlainText -Force
 
 $hash=@{
@@ -55,36 +56,30 @@ switch ($PSCmdlet.ParameterSetName)
         $hash.DbType=$DBType
     }
     'Internal Database' {
-        $hash=@{
+        Write-Host "Starting internal database"
+        # Doing part of the https://github.com/Sarafian/Docker/blob/master/Source/mssql2014-server-windows-express/start.ps1
+
+        if($ACCEPT_EULA -ne "Y" -And $ACCEPT_EULA -ne "y"){
+	        Write-Verbose "ERROR: You must accept the End User License Agreement before this container can start."
+	        Write-Verbose "Set the environment variable ACCEPT_EULA to 'Y' if you accept the agreement."
+
+            exit 1 
         }
-        & .\start -sa_password $sa_password -ACCEPT_EULA $ACCEPT_EULA -attach_dbs $attach_dbs
+
+        Write-Verbose "Starting SQL Server"
+        start-service MSSQL`$SQLEXPRESS
+
+        if($sa_password -ne "_"){
+	        Write-Verbose "Changing SA login credentials"
+            $sqlcmd = "ALTER LOGIN sa with password=" +"'" + $sa_password + "'" + ";ALTER LOGIN sa ENABLE;"
+            Invoke-Sqlcmd -Query $sqlcmd -ServerInstance ".\SQLEXPRESS" 
+        }
+
+        Write-Verbose "Started SQL Server."
     }
 }
 
-if($PSCmdlet.ParameterSetName -eq "Internal Database")
-{
-    # Doing part of the https://github.com/Sarafian/Docker/blob/master/Source/mssql2014-server-windows-express/start.ps1
-
-    if($ACCEPT_EULA -ne "Y" -And $ACCEPT_EULA -ne "y"){
-	    Write-Verbose "ERROR: You must accept the End User License Agreement before this container can start."
-	    Write-Verbose "Set the environment variable ACCEPT_EULA to 'Y' if you accept the agreement."
-
-        exit 1 
-    }
-
-    Write-Verbose "Starting SQL Server"
-    start-service MSSQL`$SQLEXPRESS
-
-    if($sa_password -ne "_"){
-	    Write-Verbose "Changing SA login credentials"
-        $sqlcmd = "ALTER LOGIN sa with password=" +"'" + $sa_password + "'" + ";ALTER LOGIN sa ENABLE;"
-        Invoke-Sqlcmd -Query $sqlcmd -ServerInstance ".\SQLEXPRESS" 
-    }
-
-    Write-Verbose "Started SQL Server."
-}
-
-& $buildersPath\Initialize-ISH.Instance.ps1 @hash
+& $buildersPath\Initialize-ISH.Instance.ps1 @hash -InContainer
 
 if($Loop)
 {
@@ -100,4 +95,8 @@ if($Loop)
         $lastCheck = Get-Date 
         Start-Sleep -Seconds 2 
     }
+}
+else
+{
+    Write-Host "hostname=$HostName"
 }
