a3aab9a82edb311f18e98cd6974f7007a6d82fc5
Alex Sarafian <dev1978@outlook.com>
2017-07-27 14:45:21 +0200

    Workaround fix for ISH.13 installtool nlog limitations (#74)
    * GH-73 Temporary workaround. Modify the nlog.config file to include the process path
    
    * Gh-73 small fix
    
    * Made the workaround for Nlog.config more protected. Should not execute on a ISH.12 CD
    GH-73

--- Source/Server/Install/Uninstall-ISHDeployment.ps1
+++ Source/Server/Install/Uninstall-ISHDeployment.ps1
@@ -59,6 +59,21 @@ $scriptBlock={
     $installToolArgs=@("-Uninstall",
         "-project",$Name
         )
+
+    #region Workaround GH-73
+    $nlogConfigPath=Join-Path $CDPath "__InstallTool\nlog.config"
+    if(Test-Path -Path $nlogConfigPath)
+    {
+        if(-not(Test-Path -Path ($nlogConfigPath+".bak")))
+        {
+            $null=Copy-Item -Path $nlogConfigPath -Destination ($nlogConfigPath+".bak")
+            $nlog=Get-Content -Path $nlogConfigPath -Raw
+            $nlog=$nlog.Replace('fileName="${serviceName}.${processId}.${creationDateTime}.log"','fileName="${processname:fullName=True}.${processId}.${creationDateTime}.log"')
+            [System.IO.File]::WriteAllText($nlogConfigPath,$nlog)
+        }
+    }
+    #endregion
+
     & $installToolPath $installToolArgs
 }
 
