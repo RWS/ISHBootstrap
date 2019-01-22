a1de36fbd682bc35715e4b8cc21226f3da35ce9a
beutepa <pascal.beutels@gmail.com>
2019-01-22 10:42:04 +0100

    [Support creation of a SQL user when initializing the mock database]

--- Source/Builders/Database/Initialize-MockDatabase.ps1
+++ Source/Builders/Database/Initialize-MockDatabase.ps1
@@ -1,9 +1,13 @@
-﻿param(
-    [Parameter(Mandatory=$true)]
+﻿[CmdletBinding(DefaultParameterSetName="OSUser")]  
+param(
+    [Parameter(Mandatory=$true,ParameterSetName="OSUserAndSqlUser")]
+    [Parameter(Mandatory=$true,ParameterSetName="OSUser")]
     [string]$OSUserSqlUser,
-    [Parameter(Mandatory=$false)]
+    [Parameter(Mandatory=$true,ParameterSetName="OSUserAndSqlUser")]
+    [Parameter(Mandatory=$true,ParameterSetName="SqlSUser")]
     [string]$SqlUserName,
-    [Parameter(Mandatory=$false)]
+    [Parameter(Mandatory=$true,ParameterSetName="OSUserAndSqlUser")]
+    [Parameter(Mandatory=$true,ParameterSetName="SqlSUser")]
     [string]$SqlPassword
 )
 
@@ -11,11 +15,6 @@ $cmdletsPaths="$PSScriptRoot\..\..\Cmdlets"
 
 . "$cmdletsPaths\Helpers\Write-Separator.ps1"
 Write-Separator -Invocation $MyInvocation -Header
-
-if((-not $SqlUserName -and $SqlPassword) -or ($SqlUserName -and -not $SqlPassword))
-{
-    Throw "Both parameters, 'SqlUserName' and 'SqlPassword' need to be provided if you also want to create a SQL User."
-}
 
 $sqlServerItem=Get-ChildItem -Path "${env:ProgramFiles(x86)}\Microsoft SQL Server" -Filter "*0" |Sort-Object -Descending @{expression={[int]$_.Name}}| Select-Object -First 1
 $sqlServerPath=$sqlServerItem |Select-Object -ExpandProperty FullName
@@ -45,84 +44,84 @@ else
 {
     throw "Could not parse connection string"
 }
-Write-Host "[DEMO][SQL Server Express]:Configuring $OSUserSqlUser account"
+if(($PSCmdlet.ParameterSetName -eq "OSUser") -or ($PSCmdlet.ParameterSetName -eq "OSUserAndSqlUser"))
+{
+    Write-Host "[DEMO][SQL Server Express]:Configuring $OSUserSqlUser account"
 
-$sqlCmd = @"
-USE [master]
-GO
-CREATE LOGIN [$OSUserSqlUser] FROM WINDOWS WITH DEFAULT_DATABASE=[master]
-GO
-USE [$dbName]
-GO
-CREATE USER [$osUserSqlUser] FOR LOGIN [$osUserSqlUser]
-GO
-USE [$dbName]
-GO
-ALTER USER [$osUserSqlUser] WITH DEFAULT_SCHEMA=[dbo]
-GO
-USE [$dbName]
-GO
-ALTER ROLE [db_owner] ADD MEMBER [$OSUserSqlUser]
-GO
+    $sqlCmd = @"
+    USE [master]
+    GO
+    CREATE LOGIN [$OSUserSqlUser] FROM WINDOWS WITH DEFAULT_DATABASE=[master]
+    GO
+    USE [$dbName]
+    GO
+    CREATE USER [$osUserSqlUser] FOR LOGIN [$osUserSqlUser]
+    GO
+    USE [$dbName]
+    GO
+    ALTER USER [$osUserSqlUser] WITH DEFAULT_SCHEMA=[dbo]
+    GO
+    USE [$dbName]
+    GO
+    ALTER ROLE [db_owner] ADD MEMBER [$OSUserSqlUser]
+    GO
 "@
 
-Invoke-Sqlcmd -Query $sqlCmd
-if($SqlUserName -and $SqlPassword)
-{
-Write-Host "[DEMO][SQL Server Express]:Configuring $SqlUserName account"
+    Invoke-Sqlcmd -Query $sqlCmd
+}
 
-$sqlCmd = @"
-USE [master]
-GO
-IF NOT EXISTS 
-    (SELECT name  
-     FROM sys.server_principals
-     WHERE name = N'$SqlUserName')
-BEGIN
-    SELECT N'Creating login for: $SqlUserName'
-    CREATE LOGIN [$SqlUserName] WITH PASSWORD = N'$SqlPassword', CHECK_POLICY = OFF, CHECK_EXPIRATION = OFF, DEFAULT_DATABASE=[$dbName]
-END
-ELSE
-BEGIN
-    SELECT N'Altering login for: $SqlUserName'
-    ALTER LOGIN [$SqlUserName] WITH PASSWORD = N'$SqlPassword', CHECK_POLICY = OFF, CHECK_EXPIRATION = OFF, DEFAULT_DATABASE=[$dbName]
-END
-GO
-ALTER LOGIN [$SqlUserName] ENABLE;
-GO
-EXEC master..sp_addsrvrolemember @loginame = N'$SqlUserName', @rolename = N'sysadmin'
-GO
-USE [$dbName]
-GO
-IF NOT EXISTS (SELECT name 
-                FROM sys.database_principals
-                WHERE type = 'S' AND name = N'$SqlUserName')
-BEGIN
-    SELECT N'Creating user for: $SqlUserName'
-    CREATE USER [$SqlUserName] FOR LOGIN [$SqlUserName] WITH DEFAULT_SCHEMA=[dbo]
-END
-ELSE
-BEGIN
-    SELECT N'User already exists: $SqlUserName'
-END
-GO
-USE [$dbName]
-GO
-ALTER USER [$SqlUserName] WITH DEFAULT_SCHEMA=[dbo]
-GO
-USE [$dbName]
-GO
-ALTER ROLE [db_owner] ADD MEMBER [$SqlUserName]
-GO
+if(($PSCmdlet.ParameterSetName -eq "SqlUser") -or ($PSCmdlet.ParameterSetName -eq "OSUserAndSqlUser"))
+{
+    Write-Host "[DEMO][SQL Server Express]:Configuring $SqlUserName account"
+
+    $sqlCmd = @"
+    USE [master]
+    GO
+    IF NOT EXISTS 
+        (SELECT name  
+         FROM sys.server_principals
+         WHERE name = N'$SqlUserName')
+    BEGIN
+        SELECT N'Creating login for: $SqlUserName'
+        CREATE LOGIN [$SqlUserName] WITH PASSWORD = N'$SqlPassword', CHECK_POLICY = OFF, CHECK_EXPIRATION = OFF, DEFAULT_DATABASE=[$dbName]
+    END
+    ELSE
+    BEGIN
+        SELECT N'Altering login for: $SqlUserName'
+        ALTER LOGIN [$SqlUserName] WITH PASSWORD = N'$SqlPassword', CHECK_POLICY = OFF, CHECK_EXPIRATION = OFF, DEFAULT_DATABASE=[$dbName]
+    END
+    GO
+    ALTER LOGIN [$SqlUserName] ENABLE;
+    GO
+    EXEC master..sp_addsrvrolemember @loginame = N'$SqlUserName', @rolename = N'sysadmin'
+    GO
+    USE [$dbName]
+    GO
+    IF NOT EXISTS (SELECT name 
+                    FROM sys.database_principals
+                    WHERE type = 'S' AND name = N'$SqlUserName')
+    BEGIN
+        SELECT N'Creating user for: $SqlUserName'
+        CREATE USER [$SqlUserName] FOR LOGIN [$SqlUserName] WITH DEFAULT_SCHEMA=[dbo]
+    END
+    ELSE
+    BEGIN
+        SELECT N'User already exists: $SqlUserName'
+    END
+    GO
+    USE [$dbName]
+    GO
+    ALTER USER [$SqlUserName] WITH DEFAULT_SCHEMA=[dbo]
+    GO
+    USE [$dbName]
+    GO
+    ALTER ROLE [db_owner] ADD MEMBER [$SqlUserName]
+    GO
 "@
 
-Invoke-Sqlcmd -Query $sqlCmd
+    Invoke-Sqlcmd -Query $sqlCmd
 }
-else
-{
-    #Throw (or is logging a warning enough?)
-    throw "Both parameters SqlUserName and SqlPassword need to be provided."
-}
+
 Pop-Location -StackName SQL
 
 Write-Separator -Invocation $MyInvocation -Footer
