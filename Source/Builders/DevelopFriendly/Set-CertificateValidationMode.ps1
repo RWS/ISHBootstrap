32d3b908d720cccfd47d8d959594a5b77252e176
beutepa <pascal.beutels@gmail.com>
2019-04-03 16:45:09 +0200

    GH-101 DevelopFriendly - Improve PowerShell scripts to help in troubleshooting instances

--- Source/Builders/DevelopFriendly/Set-CertificateValidationMode.ps1
+++ Source/Builders/DevelopFriendly/Set-CertificateValidationMode.ps1
@@ -1,60 +1,40 @@
 
 param(
-    [Parameter(Mandatory=$false)]
+    [Parameter(Mandatory = $false)]
     [string]$certificateValidationMode = "None"
 
 )
 
 
-$cmdletsPaths="$PSScriptRoot\..\..\Cmdlets"
+$cmdletsPaths = "$PSScriptRoot\..\..\Cmdlets"
 
 . "$cmdletsPaths\Helpers\Write-Separator.ps1"
 . "$cmdletsPaths\Helpers\Get-ProgressHash.ps1"
 
 Write-Separator -Invocation $MyInvocation -Header
-$scriptProgress=Get-ProgressHash -Invocation $MyInvocation
+$scriptProgress = Get-ProgressHash -Invocation $MyInvocation
 
-
-$blockName="[DEVELOPFRIENDLY][Windows Server]:Changing certificateValidationMode"
+$blockName = "[DEVELOPFRIENDLY][Windows Server]:Changing certificateValidationMode"
 Write-Host $blockName
 Write-Warning "$blockName - ONLY USE THIS FOR A LOCAL DEVELOPMENT INSTANCE!"
 
 
-$blockName="Getting deployment information"
+$blockName = "Getting deployment information: website name"
 Write-Progress @scriptProgress -Status $blockName
 Write-Host $blockName
 
-$deploymentParameters = Get-ISHDeploymentParameters # -ISHDeployment $ishDeploymentName
+$websitename = Get-ISHDeploymentParameters | Where-Object -Property Name -EQ websitename | Select-Object -ExpandProperty Value
+Write-Verbose "Website name: $websitename" 
 
-$paramWebPath = $deploymentParameters |Where-Object -Property Name -EQ "webpath"|Select-Object -ExpandProperty Value
-$paramProjectSuffix = $deploymentParameters |Where-Object -Property Name -EQ "projectsuffix"|Select-Object -ExpandProperty Value
-
-
-$webPath = "$paramWebPath\Web$paramProjectSuffix\Author\ASP"
-$webConfig = "web.config"
-$fullISHCMPath = "$webPath\$webConfig"
-Write-Verbose "ISHCM web.config: $fullISHCMPath"
-
-$webPath = "$paramWebPath\Web$paramProjectSuffix\InfoShareCE"
-$webConfig = "web.config"
-$fullISHCEPath = "$webPath\$webConfig"
-Write-Verbose "ISHCE web.config: $fullISHCEPath"
-
-$webPath = "$paramWebPath\Web$paramProjectSuffix\InfoShareWS"
-$webConfig = "web.config"
-$fullISHWSPath = "$webPath\$webConfig"
-Write-Verbose "ISHSTS web.config: $fullISHWSPath"
-
-$fullPaths = @($fullISHCMPath,$fullISHCEPath,$fullISHWSPath)
-
-
-$blockName="Processing all web.config files"
+$blockName = "Processing all web.config files, except InfoShareSTS"
 Write-Progress @scriptProgress -Status $blockName
 Write-Host $blockName
 
-foreach($fullPath in $fullPaths)
-{
-    $blockName="Updating $fullPath - setting certificateValidationMode to: $certificateValidationMode"
+Get-ISHDeploymentParameters | Where-Object -Property Name -Like "infoshare*webappname" | Where-Object -Property Name -NE "infosharestswebappname" | ForEach-Object {
+    $ISSWebFullAppName = "IIS:\Sites\$($websitename)\$($_.Value)"
+    $fullPath = "$($(Get-WebFilePath $ISSWebFullAppName).FullName)\web.config"
+
+    $blockName = "Updating $fullPath - setting certificateValidationMode to: $certificateValidationMode"
     Write-Progress @scriptProgress -Status $blockName
     Write-Host $blockName
     [xml]$webConfigXML = Get-Content $fullPath
